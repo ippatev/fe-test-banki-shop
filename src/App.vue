c60b5ea995c2350c4320f8bb8684f9c7d9090ecd
Aleksandr Ippatev <alexanderippatiev24@gmail.com>
2025-02-23 13:09:16 +0900

    feat: add header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TheHeader></TheHeader>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TheHeader
   }
 }
 </script>
 
 <style>
+:root {
+  --black: #343030;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Merriweather, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: var(--black);
+  font-size: 14px;
+}
+
+h1 {
+  font-size: 24px;
+  font-weight: bold;
+}
+
+h2 {
+  font-size: 18px;
+  font-weight: normal;
+}
+
+h3 {
+  font-size: 16px;
+  font-weight: bold;
+}
+
+h4,
+h5,
+h6 {
+  font-size: 14px;
+
+}
+
+h4 {
+  font-weight: bold;
+}
+
+h5 {
+  font-weight: bold;
+}
+
+h6 {
+  font-weight: lighter;
 }
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header">
+    <ul>
+      <li>Каталог</li>
+      <li>Доставка</li>
+      <li>Оплата</li>
+      <li>Контакты</li>
+      <li>О компании</li>
+    </ul>
+    <div class="header__search-bar">
+      <input type="text" value="" placeholder="Поиск по названию картины" class="header__search-input" />
+      <button class="header__search-button">Найти</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: nowrap;
+}
+
+.header ul {
+  display: flex;
+  gap: 48px;
+}
+</style>
